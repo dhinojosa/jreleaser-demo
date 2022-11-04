a10c9490bf8146754d7bb8511974c209430b3485
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2022-11-04 18:02:34 -0500

    Release 0.0.9 preparations

--- src/main/java/com/xyzcorp/Runner.java
+++ src/main/java/com/xyzcorp/Runner.java
@@ -3,6 +3,6 @@ package com.xyzcorp;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Welcome to the JReleaser Application");
-        System.out.println("Enjoy your stay");
+        System.out.println("Enjoy your stay!!");
     }
 }
